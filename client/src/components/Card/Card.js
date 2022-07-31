cbbc03156c90e2dea4c2fc9d0720dc32c3ae10e0
Mario <mario.caballero.948@gmail.com>
2022-07-31 19:57:35 -0300

    Styles for card was add

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,27 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { capFirstLet } from "../Auxiliaries/Auxiliaries";
 import style from "./Card.module.css";
+import { typ } from "./CSS Types/auxiliar";
 
 function Card({ name, img, id, types }) {
   return (
     <div className={style.card}>
-      <h2>#{id}</h2>
-      <img alt={name} src={img} style={{ width: 150, height: 150 }} />
-      <div>
-        <Link to={`/home/pokemon/${id}`}>{capFirstLet(name)}</Link>
+      <div className={typ(types)}>
+        <Link className={style.link} to={`/home/pokemon/${id}`}>
+          {name.toUpperCase()}
+        </Link>
+        <h2 className={style.h2}>#{id}</h2>
+      </div>
+      <div className={style.divImg}>
+        <img alt={name} src={img} className={style.img} />
+      </div>
+      <div className={typ(types)}>
         {types.map((typ, i) => (
-          <img
-            /*style={{
-              border: "solid gray",
-              backgroundColor: "red",
-              borderRadius: 50,
-              color: "white;",
-              width: 150,
-            }}*/
-            key={i}
-            alt={typ.name}
-            src={typ.img}
-          />
+          <img key={i} alt={typ.name} src={typ.img} className={style.typeImg} />
         ))}
       </div>
     </div>
