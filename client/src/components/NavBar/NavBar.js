319f81e6ffc50182d9ad93622025a747f745c667
Mario <mario.caballero.948@gmail.com>
2022-08-02 20:13:28 -0300

    V3.0.6 add NavBar style

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "./NavBar.module.css";
+import pokemon from "../Imgs/pokemon5.png";
+import newpoke from "../Imgs/newPokemon.png";
 
 function NavBar() {
   return (
     <div className={style.nav}>
-      <Link to={"/home"}>Pokemons</Link>
-      <Link to={"/home/newpokemon"}>Create</Link>
+      <Link to={"/home"}>
+        <img alt="pokemon" src={pokemon} className={style.pokemon} />
+      </Link>
+      <Link to={"/home/newpokemon"} className={style.create}>
+        <h3 className={style.h3}>NEW</h3>
+        <img alt="newPoke" src={newpoke} className={style.newpoke}/>
+      </Link>
     </div>
   );
 }
