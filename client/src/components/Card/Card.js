174726f5f8719ddb41ef059b1351152770db0203
Mario <mario.caballero.948@gmail.com>
2022-08-06 11:20:49 -0300

    V3.6.0 fix image default on card

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import style from "./Card.module.css";
 import { typ } from "./CSS Types/auxiliar";
+import pokeball from "../Imgs/pokeball3.png";
 
 function Card({ name, img, id, types }) {
   let newId;
@@ -21,7 +22,11 @@ function Card({ name, img, id, types }) {
         <h2 className={style.h2}>#{newId}</h2>
       </div>
       <div className={style.divImg}>
-        <img alt={name} src={img} className={style.img} />
+        {img ? (
+          <img alt={name} src={img} className={style.img} />
+        ) : (
+          <img alt="pokeball" src={pokeball} className={style.img} />
+        )}
       </div>
       <div className={typ(types)}>
         {types.map((typ, i) => (
