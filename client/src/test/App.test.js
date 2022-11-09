694d93605cdc8697997534a28cd0fd7151cebf71
vanegabioudclear <vanesagabioud@yahoo.com.ar>
2022-11-09 18:09:17 -0300

    add app and navbar test

--- client/src/test/App.test.js
+++ client/src/test/App.test.js
@@ -1,9 +1,135 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { configure, mount } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { MemoryRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import configureStore from "redux-mock-store";
+
 import App from "../App";
+import NavBar from "../components/NavBar/NavBar";
+import Home from "../components/Home copy/Home2";
+import CardDetail from "../components/CardDetail/CardDetail";
+import CardCreate from "../components/CardCreate/CardCreate";
+import LandingPage from "../components/LandingPage/LandingPage";
+
+configure({ adapter: new Adapter() });
 
 xdescribe("<App />", () => {
-  it("should first", () => {
-    const component = render(<App />);
+  let store;
+  const routes = ["/", "/home", "/home/pokemon/1", "/home/newpokemon"];
+  const mockStore = configureStore([thunk]);
+  const state = {
+    pokemons: {
+      pokemons: [
+        { name: "mario", img: "", id: 1, types: [{ name: "fire", id: 1 }] },
+      ],
+    },
+    types: { types: [{ name: "fire", id: 1 }] },
+    pokemon: {
+      pokemon: [
+        { name: "mario", img: "", id: 1, types: [{ name: "fire", id: 1 }] },
+      ],
+    },
+  };
+
+  store = mockStore(state);
+
+  const componentToUse = (route) => {
+    return (
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[route]}>
+          <App />
+        </MemoryRouter>
+      </Provider>
+    );
+  };
+
+  describe("<LandingPage />", () => {
+    it('should be only render on path "/"', () => {
+      const app = mount(componentToUse(routes[0]));
+      expect(app.find(CardDetail)).toHaveLength(0);
+      expect(app.find(CardCreate)).toHaveLength(0);
+      expect(app.find(Home)).toHaveLength(0);
+      expect(app.find(NavBar)).toHaveLength(0);
+      expect(app.find(LandingPage)).toHaveLength(1);
+    });
+
+    it("shouldn't be render in any other route", () => {
+      const app = mount(componentToUse(routes[1]));
+      expect(app.find(LandingPage)).toHaveLength(0);
+
+      const app2 = mount(componentToUse(routes[2]));
+      expect(app2.find(LandingPage)).toHaveLength(0);
+
+      const app3 = mount(componentToUse(routes[3]));
+      expect(app3.find(LandingPage)).toHaveLength(0);
+    });
+  });
+
+  describe("<NavBar />", () => {
+    it('should be render on path "/home"', () => {
+      const app = mount(componentToUse(routes[1]));
+      expect(app.find(NavBar)).toHaveLength(1);
+    });
+
+    it('should be render on path "/home/pokemon/:id"', () => {
+      const app = mount(componentToUse(routes[2]));
+      expect(app.find(NavBar)).toHaveLength(1);
+    });
+
+    it('should be render on path "/home/newpokemon"', () => {
+      const app = mount(componentToUse(routes[3]));
+      expect(app.find(NavBar)).toHaveLength(1);
+    });
+
+    it("shouldn't be render on path '/'", () => {
+      const app = mount(componentToUse(routes[0]));
+      expect(app.find(NavBar)).toHaveLength(0);
+    });
+  });
+
+  describe("<Home />", () => {
+    it('should be only render on path "/home"', () => {
+      const app = mount(componentToUse(routes[1]));
+      expect(app.find(CardDetail)).toHaveLength(0);
+      expect(app.find(CardCreate)).toHaveLength(0);
+      expect(app.find(LandingPage)).toHaveLength(0);
+      expect(app.find(Home)).toHaveLength(1);
+      expect(app.find(NavBar)).toHaveLength(1);
+    });
+
+    it("shouldn't be render in any other route", () => {
+      const app = mount(componentToUse(routes[0]));
+      expect(app.find(Home)).toHaveLength(0);
+
+      const app2 = mount(componentToUse(routes[2]));
+      expect(app2.find(Home)).toHaveLength(0);
+
+      const app3 = mount(componentToUse(routes[3]));
+      expect(app3.find(Home)).toHaveLength(0);
+    });
+  });
+
+  describe("<CardDetail />", () => {
+    it('the route "/home/pokemon/:id" should be render only CardDetail', () => {
+      const app = mount(componentToUse(routes[2]));
+      expect(app.find(Home)).toHaveLength(0);
+      expect(app.find(CardCreate)).toHaveLength(0);
+      expect(app.find(LandingPage)).toHaveLength(0);
+      expect(app.find(CardDetail)).toHaveLength(1);
+      expect(app.find(NavBar)).toHaveLength(1);
+    });
+  });
+
+  describe("<CardCreate />", () => {
+    it('the route "/home/newpokemon" should be render only CardCreate"', () => {
+      const app = mount(componentToUse(routes[3]));
+      expect(app.find(CardCreate)).toHaveLength(1);
+      expect(app.find(NavBar)).toHaveLength(1);
+      expect(app.find(CardDetail)).toHaveLength(0);
+      expect(app.find(Home)).toHaveLength(0);
+      expect(app.find(LandingPage)).toHaveLength(0);
+    });
   });
 });
--- client/src/test/NavBar.test.js
+++ client/src/test/NavBar.test.js
@@ -1,12 +1,22 @@
 import React from "react";
-import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { Link } from "react-router-dom";
+import { configure, shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+
 import NavBar from "../components/NavBar/NavBar";
 
-describe("<NavBar />", ()=>{
-    it("should", ()=>{
-        const component = render(<NavBar />)
+configure({ adapter: new Adapter() });
 
-        component.getAllByRole("button")
-    })
-})
+xdescribe("<NavBar />", () => {
+  const nav = shallow(<NavBar location={{ pathname: "/home" }} />);
+
+  it('should be render two <Link to="" />. The first go to "/home", and the second go to "/home/newpokemon"', () => {
+    expect(nav.find(Link).length).toBeGreaterThanOrEqual(2);
+    expect(nav.find(Link).at(0).prop("to")).toEqual("/home");
+    expect(nav.find(Link).at(1).prop("to")).toEqual("/home/newpokemon");
+  });
+
+  it("should be render a button", () => {
+    expect(nav.find("button").length).toEqual(1);
+  });
+});
